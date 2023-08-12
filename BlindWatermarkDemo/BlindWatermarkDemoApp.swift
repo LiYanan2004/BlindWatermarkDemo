b93848e10d93fef292b425e2c00faedaa529c5c8
LiYanan2004 <1474700628@qq.com>
2023-08-12 10:46:55 +0800

    Initial Commit

---
+++ BlindWatermarkDemo/BlindWatermarkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlindWatermarkDemoApp.swift
+//  BlindWatermarkDemo
+//
+//  Created by LiYanan2004 on 2023/8/12.
+//
+
+import SwiftUI
+
+@main
+struct BlindWatermarkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlindWatermarkDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlindWatermarkDemo
+//
+//  Created by LiYanan2004 on 2023/8/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
